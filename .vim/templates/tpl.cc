4619fe8c99778633bc7fb8ccf2367ff63bd6861c
reallytrue1262 <reallytrue1262@gmail.com>
2013-03-17 16:52:15 +0800

    test

---
+++ .vim/templates/tpl.cc
@@ -0,0 +1,13 @@
+/*
+ * @ Project : g3suh
+ * @ File name : <T_FILENAME>
+ * @ Date : <T_CREATE_DATE>
+ * @ Author : <T_AUTHOR> (<T_AUTHOR_EMAIL>)
+ */
+
+#if !defined(<T_FILENAME>)
+#define <T_FILENAME>
+
+
+#endif // <T_FILENAME>
+ 
